9e757100805d0712d7b3590590ccf327507fe4d5
James Roncesvalles <JamesRoncy13@gmail.com>
2018-06-01 10:48:32 +0800

    express: added comment lines

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+// added comments
 app.get('/', (req, res) => res.send('hello world'))
 
 app.listen(3000, () => console.log('Run running on port 3000!'))
