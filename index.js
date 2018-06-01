8088cc350fb438fa0a243656b421d362fd5bde5e
Test Account <karagatangpasipiko@yahoo.com>
2018-06-01 09:34:46 +0800

    express: implemented express on the application

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-//added sample messages
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('hello world'))
+
+app.listen(3000, () => console.log('Run running on port 3000!'))
