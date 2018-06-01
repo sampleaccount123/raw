47e9737d5fc4f72985b8e914cd4ec5c351e96312
James Roncesvalles <JamesRoncy13@gmail.com>
2018-06-01 11:14:37 +0800

    added second hello get

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const express = require('express')
 const app = express()
 // added comments
 app.get('/', (req, res) => res.send('hello world'))
+app.get('/hello', (req, res) => res.send('hello'))
 
-app.listen(3000, () => console.log('Run running on port 3000!'))
+
+app.listen(4000, () => console.log('Run running on port 3000!'))
