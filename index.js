af47f2dd27cb14808bfe01f08cfb258609ce42ce
James Roncesvalles <JamesRoncy13@gmail.com>
2018-06-01 10:19:40 +0800

    feature-login-functionality: implemented login post response

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('hello world'))
+const bodyParser = require('body-parser');
+
+// Create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static(__dirname + '/views'));
+
+app.get('/', (req, res) => res.render('index'))
+
+app.post('/login', urlencodedParser, (req, res) => {
+  var username = req.body.username,
+   password = req.body.password
+  res.send(`${username} ${password}`)
+})
 
 app.listen(3000, () => console.log('Run running on port 3000!'))
